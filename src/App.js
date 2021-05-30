dc8b6504b890664f38bfbf066c0400d925a2caed
Slav Lazarevic <slav.lazarevic@myob.com>
2021-05-30 23:16:36 +1000

    Renamed Main component import

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import Provider from './context/Provider';
-import Home from './modules/Main';
+import Main from './modules/Main';
 import theme from './theme';
 
 export const App = () => (
   <Provider>
     <ThemeProvider theme={theme}>
-      <Home />
+      <Main />
     </ThemeProvider>
   </Provider>
 );
